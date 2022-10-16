835828256339884c27aa255b7982826a1b486ee1
natrix_dev <discordlenko@gmail.com>
2022-10-16 20:22:18 +0200

    Create natrix_booster.py

---
+++ natrix_booster.py
@@ -0,0 +1,18 @@
+import requests; import threading; import time;
+
+from discord import * 
+
+accounts = "https://github.com/natrixdev/Discord-Server-Boost-Botter/blob/main/config/accounts/accounts.txt"
+
+minConfig = "https://github.com/natrixdev/Discord-Server-Boost-Botter/tree/main/config"
+
+discord_server_config = "https://github.com/natrixdev/Discord-Server-Boost-Botter/blob/main/config/server.json"
+
+join = 'req.on("discord.gg/server.UID")'
+boost = 'req.on("https://discord.com/channel:config.serverID/boost:method=autoBoostbyNitro/pay/accept/boost/2/true/uid=?17814111718178564")'
+while True: 
+    accounts = accounts.filter="https://github.com/natrixdev/Discord-Server-Boost-Botter/blob/main/config/accounts/acc.py"
+        for accounts in discord_server_config:
+            discord_server_config =x 
+            x.join()
+            x.boost()
